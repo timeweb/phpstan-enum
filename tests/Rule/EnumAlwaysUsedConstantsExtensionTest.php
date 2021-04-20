36da7afc2adc06a494078d3a0a6ec32e3aac0394
Jérémy DECOOL <contact@jdecool.fr>
2021-04-20 13:24:03 +0200

    Add EnumAlwaysUsedConstantsExtension

---
+++ tests/Rule/EnumAlwaysUsedConstantsExtensionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Timeweb\Tests\PHPStan\Rule;
+
+use PHPStan\Testing\TestCase;
+use Timeweb\PHPStan\Rule\EnumAlwaysUsedConstantsExtension;
+use Timeweb\Tests\PHPStan\Fixture\EnumFixture;
+
+/**
+ * @coversDefaultClass \Timeweb\PHPStan\Rule\EnumAlwaysUsedConstantsExtension
+ */
+class EnumAlwaysUsedConstantsExtensionTest extends TestCase
+{
+    /**
+     * @var \PHPStan\Broker\Broker
+     */
+    protected $broker;
+
+    /**
+     * @var EnumAlwaysUsedConstantsExtension
+     */
+    protected $constantsExtension;
+
+    public function setUp(): void
+    {
+        $this->broker = $this->createBroker();
+        $this->constantsExtension = new EnumAlwaysUsedConstantsExtension();
+    }
+
+    /**
+     * @covers ::isAlwaysUsed
+     * @dataProvider enumFixtureProperties
+     */
+    public function testEnumConstantsAreConsideredAsAlwaysUsed(string $constantName): void
+    {
+        $classReflection = $this->broker->getClass(EnumFixture::class);
+        $constantReflection = $classReflection->getConstant($constantName);
+
+        $this->assertTrue($this->constantsExtension->isAlwaysUsed($constantReflection));
+
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function enumFixtureProperties(): array
+    {
+        return [
+            ['MEMBER'],
+            ['PUBLIC_CONST'],
+            ['PRIVATE_CONST'],
+        ];
+    }
+}
