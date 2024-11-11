9948ee3dbd3e34a0addb11929783156b41536ab9
Jérémy DECOOL <contact@jdecool.fr>
2024-11-11 20:51:54 +0100

    Upgrade code to PHPStan 2.x

--- src/Rule/EnumAlwaysUsedConstantsExtension.php
+++ src/Rule/EnumAlwaysUsedConstantsExtension.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Timeweb\PHPStan\Rule;
 
 use MyCLabs\Enum\Enum;
-use PHPStan\Reflection\ConstantReflection;
+use PHPStan\Reflection\ClassConstantReflection;
 use PHPStan\Rules\Constants\AlwaysUsedClassConstantsExtension;
 
 class EnumAlwaysUsedConstantsExtension implements AlwaysUsedClassConstantsExtension
 {
-    public function isAlwaysUsed(ConstantReflection $constant): bool
+    public function isAlwaysUsed(ClassConstantReflection $constant): bool
     {
         return $constant->getDeclaringClass()->isSubclassOf(Enum::class);
     }
